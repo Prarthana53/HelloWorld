3bb3fe833ed40c33551718f0acf8967e0af81647
hanas <prathnapradhan@yahoo.com>
2021-03-11 18:38:23 -0600

    First Commit of automation.

---
+++ src/Automation/Automation.java
@@ -0,0 +1,8 @@
+package Automation;
+
+public class Automation {
+    public static void main(String[] args) {
+        System.out.println("Automation");
+        System.out.println("Prarthana Pradhan");
+    }
+}
